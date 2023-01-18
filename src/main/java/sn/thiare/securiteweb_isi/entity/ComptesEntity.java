a551a1f366e3246cdd550b1c51d41c38e9ea44ec
thiareMohamed <thiaremohamed.mt@gmail.com>
2023-01-18 13:35:54 +0000

    feat: add entities droit and compte

---
+++ src/main/java/sn/thiare/securiteweb_isi/entity/ComptesEntity.java
@@ -0,0 +1,57 @@
+package sn.thiare.securiteweb_isi.entity;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "comptes", schema = "securiteweb_isi")
+public class ComptesEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "email")
+    private String email;
+    @Basic
+    @Column(name = "password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ComptesEntity that = (ComptesEntity) o;
+        return id == that.id && Objects.equals(email, that.email) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password);
+    }
+}
---
+++ src/main/java/sn/thiare/securiteweb_isi/entity/DroitEntity.java
@@ -0,0 +1,46 @@
+package sn.thiare.securiteweb_isi.entity;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "droit", schema = "securiteweb_isi")
+public class DroitEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DroitEntity that = (DroitEntity) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
